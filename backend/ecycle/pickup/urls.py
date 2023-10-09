69263617c3f832f77940019fc82feadc68258700
loneranger455-maker <rangerlone547@gmail.com>
2023-10-09 20:03:45 +0545

    fix: issue with pickup id fixed

--- backend/ecycle/pickup/urls.py
+++ backend/ecycle/pickup/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('all/<str:pickup_id>/', PickupView.as_view()),
+    path('all/', PickupView.as_view()),
     path('pickup/<str:pickup_id>/', PickupView.as_view()),
     path('amenity/all/',GetAmenity.as_view())
 
