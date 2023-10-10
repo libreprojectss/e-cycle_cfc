1bc42e4c3902fae4194b0005beaeb224cf3a449f
Tilakkhatri <khatritilak956@gmail.com>
2023-10-10 21:30:07 +0545

    rem:removed package

--- admin_ui/src/main.jsx
+++ admin_ui/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "../src/context/userContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <ChakraProvider>
